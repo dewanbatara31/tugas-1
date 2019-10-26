85f1ebe14965de91bb4cd1055a77285b2877a7a5
dewanbatara31 <56387936+dewanbatara31@users.noreply.github.com>
2019-10-26 15:44:52 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,14 @@
+namalengkap = "Dewan Batara Subardjah"
+namapanggilan = "Dewan"
+npm = "311910593"
+tempatlahir = "Bekasi"
+umur = "19"
+telp = "0895342207896"
+alamat = "kp.pamahan"
+
+print("assalamualaikum,\n"
+      "let me my self, my name is",namalengkap,
+      "but you can call me",namapanggilan,
+      ", my npm is",npm,
+      ", i was born in",tempatlahir,
+      "and i am",umur,"years old. i am very glad if you want to invite my house in",alamat,", so don't forget to call me before wit the number",telp,"\n thank you")
